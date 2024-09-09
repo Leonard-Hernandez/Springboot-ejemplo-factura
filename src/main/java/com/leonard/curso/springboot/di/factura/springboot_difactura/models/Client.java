a6e568c81664fe9616e9ef0a89ff19f0e925d4d9
Leonard <Leonardhernandez02@gmail.com>
2024-09-08 19:13:26 -0500

    created the relation between the componets

--- src/main/java/com/leonard/curso/springboot/di/factura/springboot_difactura/models/Client.java
+++ src/main/java/com/leonard/curso/springboot/di/factura/springboot_difactura/models/Client.java
@@ -1,5 +1,35 @@
 package com.leonard.curso.springboot.di.factura.springboot_difactura.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Client {
+
+    private String name;
+    private String LastName;
+
+    private Client() {
+    }
+
+    public Client(String name, String lastName) {
+        this.name = name;
+        LastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
 
 }
