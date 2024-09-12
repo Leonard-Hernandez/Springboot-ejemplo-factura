9c0d2d5bed7cd0fa787fffebba514601548c1158
Leonard <Leonardhernandez02@gmail.com>
2024-09-11 21:48:59 -0500

    choice one of the two options to items in the invoice

--- src/main/java/com/leonard/curso/springboot/di/factura/springboot_difactura/AppConfig.java
+++ src/main/java/com/leonard/curso/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -15,11 +15,21 @@ import com.leonard.curso.springboot.di.factura.springboot_difactura.models.Produ
 public class AppConfig {
 
     @Bean
-    List<Item> intemsInvoice(){
+    List<Item> itemsInvoice() {
         Product p1 = new Product("camara sony", 200.0);
         Product p2 = new Product("bicicleta", 500.0);
-        
+
         return Arrays.asList(new Item(p1, 2), new Item(p2, 4));
     }
 
+    @Bean("default")
+    List<Item> itemsInvoiceOffice() {
+        Product p1 = new Product("Pisa papeles", 20.0);
+        Product p2 = new Product("Impresora hp", 800.0);
+        Product p3 = new Product("Laptop hp", 1500.0);
+        Product p4 = new Product("Laptop dell", 2000.0);
+
+        return Arrays.asList(new Item(p1, 3), new Item(p2, 5),
+                new Item(p3, 1), new Item(p4, 2));
+    }
 }
