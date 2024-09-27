49b1caa27752fb21b8da875ed081454ea449269f
leonard david hernandez parra <leonardhernandez02@gmail.com>
2024-09-27 11:14:20 -0500

    reforzando los ejemplos de scopes

--- src/main/java/com/leonard/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
+++ src/main/java/com/leonard/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.leonard.curso.springboot.di.factura.springboot_difactura.models.Client;
 import com.leonard.curso.springboot.di.factura.springboot_difactura.models.Invoice;
 
 @RestController
@@ -16,6 +17,15 @@ public class InvoiceController {
 
     @GetMapping("/show")
     public Invoice show() {
+        Invoice i = new Invoice();
+        Client c = new Client();
+
+        c.setName(invoice.getClient().getName());
+        c.setLastName(invoice.getClient().getLastName());
+        
+        i.setClient(c);
+        i.setDescription(invoice.getDescription());
+        i.setItems(invoice.getItems());
         return invoice;
     }
 
